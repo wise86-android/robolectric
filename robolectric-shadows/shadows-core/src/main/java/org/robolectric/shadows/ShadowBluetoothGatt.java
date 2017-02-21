7df419b14b176c49b4dfe3344ab0b215b90cfcde
Wise86 <wise86android@gmail.com>
2017-02-21 23:14:12 +0100

    impement read write characteristics

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowBluetoothGatt.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowBluetoothGatt.java
@@ -2,10 +2,10 @@ package org.robolectric.shadows;
 
 
 import android.annotation.NonNull;
-import android.annotation.TargetApi;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 
 import org.robolectric.annotation.Implementation;
@@ -13,31 +13,44 @@ import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR2;
 
 @Implements(value = BluetoothGatt.class, minSdk = JELLY_BEAN_MR2)
 public class ShadowBluetoothGatt {
 
+    private final static long CALLBACK_DELAY =100;
+
     @RealObject private BluetoothGatt realGatt;
 
-    private boolean mServiceDiscoverd;
-    private BluetoothGattCallback userCallback;
+    private boolean mServiceDiscoverd=false;
+    private boolean mIsConnected=false;
+    private BluetoothGattCallback mUserCallback;
     private BluetoothDevice device;
 
-    private List<BluetoothGattService> deviceServices;
+    private List<BluetoothGattService> mDeviceServices;
+    private Map<UUID,Iterator<byte[]>> mCharDataGenerator = new HashMap<>();
+
 
     public void setDevice(BluetoothDevice device){
         this.device = device;
     }
 
     public void setCallback(@NonNull BluetoothGattCallback callback) {
-        userCallback=callback;
+        mUserCallback =callback;
     }
 
     public void setDeviceServices(List<BluetoothGattService> services){
-        deviceServices = services;
+        mDeviceServices = services;
+    }
+
+    public void setDataGeneratorForCharacteristics(UUID charUuid, Iterator<byte[]> dataGenerator){
+        mCharDataGenerator.put(charUuid,dataGenerator);
     }
 
     @Implementation
@@ -45,14 +58,15 @@ public class ShadowBluetoothGatt {
         runOnBackgroundThread(new Runnable() {
             @Override
             public void run() {
-                userCallback.onConnectionStateChange(realGatt,
+                mUserCallback.onConnectionStateChange(realGatt,
                         BluetoothGatt.GATT_SUCCESS, BluetoothGatt.STATE_CONNECTING);
             }
         });
         runOnBackgroundThread(new Runnable() {
             @Override
             public void run() {
-                userCallback.onConnectionStateChange(realGatt,
+                mIsConnected=true;
+                mUserCallback.onConnectionStateChange(realGatt,
                         BluetoothGatt.GATT_SUCCESS, BluetoothGatt.STATE_CONNECTED);
             }
         });
@@ -61,17 +75,19 @@ public class ShadowBluetoothGatt {
 
     @Implementation
     public void disconnect(){
+        if(!mIsConnected)
+            return;
         runOnBackgroundThread(new Runnable() {
             @Override
             public void run() {
-                userCallback.onConnectionStateChange(realGatt,
+                mUserCallback.onConnectionStateChange(realGatt,
                         BluetoothGatt.GATT_SUCCESS, BluetoothGatt.STATE_DISCONNECTING);
             }
         });
         runOnBackgroundThread(new Runnable() {
             @Override
             public void run() {
-                userCallback.onConnectionStateChange(realGatt,
+                mUserCallback.onConnectionStateChange(realGatt,
                         BluetoothGatt.GATT_SUCCESS, BluetoothGatt.STATE_DISCONNECTED);
             }
         });
@@ -84,29 +100,100 @@ public class ShadowBluetoothGatt {
 
     @Implementation
     public void discoverServices(){
+        if (!mIsConnected)
+            return;
         runOnBackgroundThread(new Runnable() {
             @Override
             public void run() {
                 mServiceDiscoverd =true;
-                userCallback.onServicesDiscovered(realGatt,BluetoothGatt.GATT_SUCCESS);
+                mUserCallback.onServicesDiscovered(realGatt,BluetoothGatt.GATT_SUCCESS);
             }
         });
+    }
+
+    private BluetoothGattService findService(UUID uuid){
+        for(BluetoothGattService s : mDeviceServices) {
+            if (s.getUuid().equals(uuid))
+                return s;
+        }
+        return null;
+    }
+
+    @Implementation
+    public BluetoothGattService	getService(UUID uuid){
+        if(mServiceDiscoverd)
+            return findService(uuid);
+        else
+            return null;
     }
 
     @Implementation
     public List<BluetoothGattService> getServices(){
         if(mServiceDiscoverd)
-            return Collections.unmodifiableList(deviceServices);
+            return Collections.unmodifiableList(mDeviceServices);
         else
             return Collections.emptyList();
     }
+
+
+    @Implementation
+    public boolean readCharacteristic(BluetoothGattCharacteristic characteristic){
+        if(!mIsConnected)
+            return false;
+
+        if((characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_READ) == 0 )
+            return false;
+
+        final Iterator<byte[]> generator = mCharDataGenerator.get(characteristic.getUuid());
+
+        final BluetoothGattCharacteristic internalCharacteristics=characteristic;
+
+        final boolean hasNext = generator.hasNext();
+        final byte[] data = hasNext ? generator.next() : null ;
+
+        runOnBackgroundThread(new Runnable() {
+            @Override
+            public void run() {
+                if(hasNext) {
+                    internalCharacteristics.setValue(data);
+                    mUserCallback.onCharacteristicRead(realGatt, internalCharacteristics,
+                            BluetoothGatt.GATT_SUCCESS);
+                }else
+                    mUserCallback.onCharacteristicRead(realGatt, internalCharacteristics,
+                            BluetoothGatt.GATT_FAILURE);
+            }
+        });
+
+
+        return true;
+    }
+
+    @Implementation
+    public boolean writeCharacteristic (final BluetoothGattCharacteristic characteristic){
+        if(!mIsConnected)
+            return false;
+
+        if((characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_WRITE) == 0 &&
+                (characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE) == 0)
+            return false;
+
+        runOnBackgroundThread(new Runnable() {
+            @Override
+            public void run() {
+                mUserCallback.onCharacteristicWrite(realGatt,characteristic,BluetoothGatt.GATT_SUCCESS);
+            }
+        });
+
+        return true;
+    }
+
 
     private static void runOnBackgroundThread(Runnable task){
         ShadowApplication app = ShadowApplication.getInstance();
         if(app == null)
             throw new RuntimeException("Impossible run background event");
 
-        app.getBackgroundThreadScheduler().post(task);
+        app.getBackgroundThreadScheduler().postDelayed(task, CALLBACK_DELAY);
     }
 
 
--- robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothGattTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothGattTest.java
@@ -1,11 +1,12 @@
 package org.robolectric.shadows;
 
-import android.annotation.TargetApi;
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,10 +16,16 @@ import org.robolectric.TestRunners;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadow.api.Shadow;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR2;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(TestRunners.MultiApiSelfTest.class)
@@ -27,7 +34,7 @@ public class ShadowBluetoothGattTest {
 
     private BluetoothGatt bluetoothGatt;
     private ShadowBluetoothGatt shadowBluetoothGatt;
-
+    private BluetoothGattCallback mCallback;
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
@@ -35,8 +42,15 @@ public class ShadowBluetoothGattTest {
     public void setUp() throws Exception {
         bluetoothGatt = Shadow.newInstanceOf(BluetoothGatt.class);
         shadowBluetoothGatt = shadowOf(bluetoothGatt);
+        
+        mCallback = mock(BluetoothGattCallback.class);
+        shadowBluetoothGatt.setCallback(mCallback);
+
     }
 
+    private static void runBacgroundTask(){
+        ShadowApplication.getInstance().getBackgroundThreadScheduler().advanceToLastPostedRunnable();
+    }
 
     @Test
     public void getDeviceReturnTheSetDevices(){
@@ -47,32 +61,28 @@ public class ShadowBluetoothGattTest {
 
     @Test
     public void connectCallTheConnectionWithConnectingState(){
-        BluetoothGattCallback callback = mock(BluetoothGattCallback.class);
-
-        shadowBluetoothGatt.setCallback(callback);
 
         bluetoothGatt.connect();
 
-        verify(callback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
+        runBacgroundTask();
+
+        verify(mCallback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
                 BluetoothGatt.STATE_CONNECTING);
-        verify(callback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
+        verify(mCallback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
                 BluetoothGatt.STATE_CONNECTED);
     }
 
     @Test
     public void disconnectCallTheConnectionWithDisconnectingState(){
-        BluetoothGattCallback callback = mock(BluetoothGattCallback.class);
 
-        shadowBluetoothGatt.setCallback(callback);
-
+        connectNode(bluetoothGatt);
         bluetoothGatt.disconnect();
-
-        verify(callback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
+        runBacgroundTask();
+        verify(mCallback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
                 BluetoothGatt.STATE_DISCONNECTING);
-        verify(callback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
+        verify(mCallback).onConnectionStateChange(bluetoothGatt,BluetoothGatt.GATT_SUCCESS,
                 BluetoothGatt.STATE_DISCONNECTED);
     }
-
 
     @Test
     public void discoverServiceIsEmptyIfNotScanned(){
@@ -81,12 +91,162 @@ public class ShadowBluetoothGattTest {
 
     @Test
     public void discoverServiceGiveCallback(){
-        BluetoothGattCallback callback = mock(BluetoothGattCallback.class);
 
-        shadowBluetoothGatt.setCallback(callback);
+        List<BluetoothGattService> services = new ArrayList<>(2);
+        services.add(mock(BluetoothGattService.class));
+        services.add(mock(BluetoothGattService.class));
+
+        shadowBluetoothGatt.setDeviceServices(services);
+
+        connectNode(bluetoothGatt);
 
         bluetoothGatt.discoverServices();
 
-        verify(callback).onServicesDiscovered(bluetoothGatt,BluetoothGatt.GATT_SUCCESS);
+        runBacgroundTask();
+
+        verify(mCallback).onServicesDiscovered(bluetoothGatt,BluetoothGatt.GATT_SUCCESS);
+
+        List<BluetoothGattService> discoverService= bluetoothGatt.getServices();
+
+        Assert.assertEquals(services.size(),discoverService.size());
+        Assert.assertEquals(services.get(0),discoverService.get(0));
+        Assert.assertEquals(services.get(1),discoverService.get(1));
     }
+
+    @Test
+    public void discoverASpecificServiceReturnNullIfNotDiscover(){
+
+        List<BluetoothGattService> services = new ArrayList<>(2);
+
+        UUID targetService = UUID.fromString("c24e3528-ef45-4513-a4b9-8cb9719a473c");
+        UUID otherService = UUID.fromString("cdc54dea-de7c-43f7-94f3-0d6131fd016a");
+
+        services.add(new BluetoothGattService(targetService,BluetoothGattService.SERVICE_TYPE_PRIMARY));
+        services.add(new BluetoothGattService(otherService,BluetoothGattService.SERVICE_TYPE_PRIMARY));
+
+        shadowBluetoothGatt.setDeviceServices(services);
+
+        Assert.assertNull(bluetoothGatt.getService(targetService));
+
+        connectNode(bluetoothGatt);
+        bluetoothGatt.discoverServices();
+
+        runBacgroundTask();
+
+        Assert.assertEquals(services.get(0),bluetoothGatt.getService(targetService));
+
+    }
+
+
+    @Test
+    public void readCharOnlyIfConnected(){
+        Assert.assertFalse(bluetoothGatt.readCharacteristic(mock(BluetoothGattCharacteristic.class)));
+    }
+
+
+    private static void connectNode(BluetoothGatt gatt){
+        gatt.connect();
+        runBacgroundTask();
+    }
+
+    @Test
+    public void readCharIsSuccessfullIfTheGeneratorHasData(){
+        UUID targetChar = UUID.fromString("c24e3528-ef45-4513-a4b9-8cb9719a473c");
+        BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(targetChar,BluetoothGattCharacteristic.PROPERTY_READ,BluetoothGattCharacteristic.PERMISSION_READ);
+
+        byte[] data = new byte[]{0x0,0x01,0x03};
+
+        Iterator<byte[]> generator = mock(Iterator.class);
+
+        when(generator.hasNext()).thenReturn(true);
+        when(generator.next()).thenReturn(data);
+
+        shadowBluetoothGatt.setDataGeneratorForCharacteristics(targetChar,generator);
+
+        connectNode(bluetoothGatt);
+
+        Assert.assertTrue(bluetoothGatt.readCharacteristic(characteristic));
+
+        runBacgroundTask();
+
+        verify(mCallback).onCharacteristicRead(bluetoothGatt,characteristic,BluetoothGatt.GATT_SUCCESS);
+        Assert.assertArrayEquals(data,characteristic.getValue());
+
+    }
+
+    @Test
+    public void readCharFailIfTheGeneratorHasNoData(){
+        UUID targetChar = UUID.fromString("c24e3528-ef45-4513-a4b9-8cb9719a473c");
+        BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(targetChar,BluetoothGattCharacteristic.PROPERTY_READ,BluetoothGattCharacteristic.PERMISSION_READ);
+
+        Iterator<byte[]> generator = mock(Iterator.class);
+
+        when(generator.hasNext()).thenReturn(false);
+
+        shadowBluetoothGatt.setDataGeneratorForCharacteristics(targetChar,generator);
+
+        connectNode(bluetoothGatt);
+
+        Assert.assertTrue(bluetoothGatt.readCharacteristic(characteristic));
+
+        runBacgroundTask();
+
+        verify(mCallback).onCharacteristicRead(bluetoothGatt,characteristic,BluetoothGatt.GATT_FAILURE);
+
+    }
+
+
+    @Test
+    public void readCharReturnFalseWithWrongProperties(){
+
+        BluetoothGattCharacteristic characteristic =  mock(BluetoothGattCharacteristic.class);
+
+        connectNode(bluetoothGatt);
+
+        when(characteristic.getProperties()).thenReturn(BluetoothGattCharacteristic.PROPERTY_NOTIFY);
+        Assert.assertFalse(bluetoothGatt.readCharacteristic(characteristic));
+
+        when(characteristic.getProperties()).thenReturn(BluetoothGattCharacteristic.PROPERTY_WRITE);
+
+        Assert.assertFalse(bluetoothGatt.readCharacteristic(characteristic));
+    }
+
+    @Test
+    public void writeCharGiveCallback(){
+        UUID targetChar = UUID.fromString("c24e3528-ef45-4513-a4b9-8cb9719a473c");
+        BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(targetChar,BluetoothGattCharacteristic.PROPERTY_WRITE,BluetoothGattCharacteristic.PERMISSION_WRITE);
+
+        connectNode(bluetoothGatt);
+
+        Assert.assertTrue(bluetoothGatt.writeCharacteristic(characteristic));
+
+        runBacgroundTask();
+
+        verify(mCallback).onCharacteristicWrite(bluetoothGatt,characteristic,BluetoothGatt.GATT_SUCCESS);
+
+    }
+
+    @Test
+    public void writeCharReturnFalseIfNotConnected(){
+        UUID targetChar = UUID.fromString("c24e3528-ef45-4513-a4b9-8cb9719a473c");
+        BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(targetChar,BluetoothGattCharacteristic.PROPERTY_WRITE,BluetoothGattCharacteristic.PERMISSION_WRITE);
+
+        Assert.assertFalse(bluetoothGatt.writeCharacteristic(characteristic));
+
+    }
+
+    @Test
+    public void writeCharReturnFalseWithWrongProperties(){
+        BluetoothGattCharacteristic characteristic =  mock(BluetoothGattCharacteristic.class);
+
+        connectNode(bluetoothGatt);
+
+        when(characteristic.getProperties()).thenReturn(BluetoothGattCharacteristic.PROPERTY_NOTIFY);
+        Assert.assertFalse(bluetoothGatt.writeCharacteristic(characteristic));
+
+        when(characteristic.getProperties()).thenReturn(BluetoothGattCharacteristic.PROPERTY_READ);
+
+        Assert.assertFalse(bluetoothGatt.writeCharacteristic(characteristic));;
+    }
+
 }
